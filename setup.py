5478f185498551cc724ab0529b410eef0b33fd60
hhuseyin@metu.edu.tr <huseyin@hacihabiboglu.org>
2019-04-11 01:14:02 +0300

    Minor changes for readthedocs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='higrid',
-    version='0.1.0',
+    version='0.1.1',
     packages=['higrid'],
     url='https://github.com/metu-sparg/higrid',
     license='BSD 3-Clause License',
